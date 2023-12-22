767a6a52e180fcd7a975a620edf753ce6f0381e2
leomarselio <leomarselio@refactory.id>
2023-12-23 00:31:44 +0700

    create prototype dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        15: '15px',
+        19: '19px',
+        75: '75px',
+      },
+      colors: {
+        primary: '#212529',
+      },
+    },
   },
   plugins: [],
-};
+}
